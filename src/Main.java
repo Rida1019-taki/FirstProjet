868bf1172867ccba5cdf3ed8981ad708af1c459b
Rida Taki <takirida72@gmail.com>
2025-11-04 16:23:21 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+import java.util.Scanner;
+
+void main() {
+    //q1
+//    Scanner input = new Scanner(System.in);
+//    System.out.print("Enter un nom: ");
+//    String name = input.next();
+//    System.out.println("Bonjour " + name);
+    //q2
+//    Scanner input = new Scanner(System.in);
+//    System.out.print("Enter un nombre : ");
+//    int nombre = input.nextInt();
+//    for(int i = 0 ; i <= nombre ; i++){
+//    System.out.println(i);
+//        }
+
+    //q3
+//    Random input = new Random();
+//    System.out.print("Enter un nombre : ");
+//    int nombre = input.nextInt(10);
+//    while (nombre == 8){
+//        System.out.println("Bravo ! Vous avez trouvé.");
+//    }
+//    nombre += 1;
+
+    //q4
+//    Scanner input = new Scanner(System.in);
+//    double solde = 1000.00;
+//    String chooce ;
+//    while (true) {
+//        System.out.println("Entrer votre choix : ");
+//        chooce = input.next();
+//        switch (chooce) {
+//            case "1":
+//                System.out.println(solde);
+//                break;
+//            case "2":
+//                System.out.println("Entrer un montant : ");
+//                double salaire = input.nextDouble();
+//                double total = solde + salaire;
+//                System.out.println(total);
+//                break;
+//            case "3":
+//                System.out.println("Entrer un montant : ");
+//                double salaireRetrait = input.nextDouble();
+//                double totalSalaire = solde - salaireRetrait;
+//                System.out.println(totalSalaire);
+//                break;
+//            case "4":
+//                System.exit(0);
+//                break;
+//            default:
+//                System.out.println("Le choix n'est pa exists.");
+//        }
+//    }
+}
+
+
