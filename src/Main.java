18297c683c9f76d4e6941df78f1750dfdb4066ae
Rida Taki <takirida72@gmail.com>
2025-11-05 14:41:38 +0100

    continue

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+
 
 void main() {
     //q1
@@ -16,13 +16,20 @@ void main() {
 //        }
 
     //q3
-//    Random input = new Random();
-//    System.out.print("Enter un nombre : ");
-//    int nombre = input.nextInt(10);
-//    while (nombre == 8){
-//        System.out.println("Bravo ! Vous avez trouvé.");
-//    }
-//    nombre += 1;
+
+//        Scanner input = new Scanner(System.in);
+//        Random random = new Random();
+//        int numr = random.nextInt(0, 10);
+//        int number;
+//        do {
+//            System.out.print("Entrez un nombre : ");
+//            number = input.nextInt();
+//            if (number != numr) {
+//                System.out.println("Essaye ");
+//            }
+//        } while (number != numr);
+//        System.out.println("Félicitations ");
+
 
     //q4
 //    Scanner input = new Scanner(System.in);
@@ -54,6 +61,45 @@ void main() {
 //                System.out.println("Le choix n'est pa exists.");
 //        }
 //    }
+
+
+    //q5
+//    int[] numbers = {1,3,5,10,9,0,4};
+//    int max = 0;
+//    int secondMax = 0;
+//    for(int i = 0 ; i < numbers.length ; i++) {
+//        if (numbers[i] > max) {
+//            secondMax = max;
+//            max = numbers[i];
+//        }
+//        else if (numbers[i] > secondMax && secondMax != max ) {
+//            secondMax = numbers[i];
+//        }
+//    }
+//    System.out.println(max);
+//    System.out.println(secondMax);
+
+
+
+    //q7
+//    int[] nombre = {2, 3, 2, 5, 3};
+//    HashMap<Integer, Integer> map = new HashMap<>();
+//    for (int i : nombre){
+//        map.put(i , map.getOrDefault(i , 0) + 1);
+//    }
+//    System.out.println(map);
+    //q8
+//    int[] nombre = {2, 6, 1, 4, 5};
+//    int[] nombre1 = {3, 7, 0, 4, 5};
+//    for (int i = 0 ; i < nombre.length ; i++){
+//        for (int j = 0 ; j < nombre.length ; j++){
+//            if (nombre[i] == nombre1[j]){
+//                System.out.println(nombre[i] + " ");
+//            }
+//        }
+//    }
+
+
 }
 
 
